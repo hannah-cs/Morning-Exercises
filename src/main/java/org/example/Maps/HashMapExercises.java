8ad2ea4d2369795f42909c1956f0481222637fa4
hcs <hannahemcs@gmail.com>
2023-10-30 11:14:51 +0100

    Monday 301023 exercise 5

--- src/main/java/org/example/Maps/HashMapExercises.java
+++ src/main/java/org/example/Maps/HashMapExercises.java
@@ -31,5 +31,20 @@ public class HashMapExercises {
         System.out.println(students.get(1));
         students.remove(1);
         System.out.println(students.get(1));
+
+        Map<String, Integer> wordCountMap = new HashMap<>();
+        String text = "This is a block of text. I am repeating some of the words in this block of text.";
+        String[] words = text.toLowerCase().split(" ");
+        for (String word : words) {
+            if (wordCountMap.containsKey(word)) {
+                wordCountMap.put(word, wordCountMap.get(word) + 1);
+            } else {
+            wordCountMap.put(word, 1);
+            }
+        }
+        for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
     }
 }
