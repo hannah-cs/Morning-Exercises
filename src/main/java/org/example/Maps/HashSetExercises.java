c4dae4853fd89e6eac1d455f1d5da8fdaa144a7c
hcs <hannahemcs@gmail.com>
2023-10-30 12:40:03 +0100

    Monday 301023, Sets exercise 3

--- src/main/java/org/example/Maps/HashSetExercises.java
+++ src/main/java/org/example/Maps/HashSetExercises.java
@@ -1,5 +1,8 @@
 package org.example.Maps;
+import com.sun.source.tree.Tree;
+
 import java.util.HashSet;
+import java.util.TreeSet;
 
 public class HashSetExercises {
     public static void main(String[] args) {
@@ -14,11 +17,26 @@ public class HashSetExercises {
             System.out.println(element);
         }
 
+
+
         HashSet<String> uniqueStrings = new HashSet();
         String[] strings = new String[]{"apple", "banana", "apple", "cherry"};
         for (String string : strings) {
             uniqueStrings.add(string);
         }
         System.out.println(uniqueStrings);
+
+
+
+        TreeSet<Integer> sortedSet = new TreeSet();
+        sortedSet.add(45);
+        sortedSet.add(57);
+        sortedSet.add(82);
+        sortedSet.add(12);
+
+        System.out.println(sortedSet);
+        for (int element : sortedSet){
+            System.out.println(element);
+        }
     }
 }
