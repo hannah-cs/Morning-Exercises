122c295c612937183734f9ea1c45562008b22898
hcs <hannahemcs@gmail.com>
2023-10-30 10:55:34 +0100

    Monday 301023 exercise 2

--- src/main/java/org/example/Maps/HashMapExercises.java
+++ src/main/java/org/example/Maps/HashMapExercises.java
@@ -12,5 +12,11 @@ public class HashMapExercises {
 
         String student1Name = students.get(1);
         System.out.println("Student 1: " + student1Name);
+
+        for (Map.Entry<Integer, String> entry : students.entrySet()) {
+            int studentID = entry.getKey();
+            String studentName = entry.getValue();
+            System.out.println("Student ID: " + studentID + ", student name: " + studentName);
+        }
     }
 }
