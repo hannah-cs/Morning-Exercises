ec2d02b16285ded337d54a38126900e7425aae14
hcs <hannahemcs@gmail.com>
2023-11-09 11:40:50 +0100

    Added more animals without changing animal class

--- src/main/java/org/example/SOLID/OExercise1.java
+++ src/main/java/org/example/SOLID/OExercise1.java
@@ -24,10 +24,27 @@ public class OExercise1 {
             }
         }
 
+    public static class Frog extends Animal {
+        public Frog() {
+            sound = "ribbit";
+            animalName = "frog";
+        }
+    }
+    public static class Horse extends Animal {
+        public Horse() {
+            sound = "neigh";
+            animalName = "horse";
+        }
+    }
+
     public static void main(String[] args) {
         Animal dog = new Dog();
         Animal cat = new Cat();
+        Animal frog = new Frog();
+        Animal horse = new Horse();
         dog.makeSound();
         cat.makeSound();
+        frog.makeSound();
+        horse.makeSound();
     }
 }
