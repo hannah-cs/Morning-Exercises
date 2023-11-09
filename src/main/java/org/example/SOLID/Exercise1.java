8f2c80fd0e3d10a286094e4be8b4e0ebda7bba94
hcs <hannahemcs@gmail.com>
2023-11-09 10:52:02 +0100

    Single responsibility exercise 1

---
+++ src/main/java/org/example/SOLID/Exercise1.java
@@ -0,0 +1,19 @@
+package org.example.SOLID;
+public class Exercise1 {
+    public static class DogSound {
+        public void makeSound(int volume) {
+            System.out.println("Dog barked at " + volume + " decibels.");
+        }
+    }
+    public static class DogEat {
+        public void eatFood(String food) {
+            System.out.println("Dog ate some " + food);
+        }
+    }
+    public static void main(String[] args) {
+        DogSound dogSound = new DogSound();
+        DogEat dogEat = new DogEat();
+        dogSound.makeSound(14);
+        dogEat.eatFood("dog food");
+    }
+}
